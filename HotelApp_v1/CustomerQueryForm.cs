5120a0a5ca97fa5e489567dc61338f81703dfc5e
eliseomrico <112983793+eliseomrico@users.noreply.github.com>
2022-11-11 12:50:45 -0600

    Finished Customer Add Feature

--- HotelApp_v1/CustomerQueryForm.cs
+++ HotelApp_v1/CustomerQueryForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,11 @@ namespace HotelApp_v1
 {
     public partial class CustomerQueryForm : UserControl
     {
+        
+        public SqlConnection DBConnection;
+
+
+
         public CustomerQueryForm()
         {
             InitializeComponent();
@@ -28,11 +34,13 @@ namespace HotelApp_v1
         }
         private void changeTextBoxesReadOnlyStatus(bool enable) // makes text boxes read-only or not read-only
         {
-            comboBox_cust_id.Text = "";
             textBox_cust_fname.ReadOnly = enable;
             textBox_cust_lname.ReadOnly = enable;
-            textBox_cust_phone_num.ReadOnly = enable;
-            textBox_cust_cc_num.ReadOnly = enable;
+            textBox_cust_phone.ReadOnly = enable;
+            textBox_cc_part1.ReadOnly = enable;
+            textBox_cc_part2.ReadOnly = enable;
+            textBox_cc_part3.ReadOnly = enable;
+            textBox_cc_part4.ReadOnly = enable;
             textBox_cust_balance.ReadOnly = enable;
         }
         private void emptyTextBoxes() // clears text from text boxes
@@ -40,9 +48,48 @@ namespace HotelApp_v1
             comboBox_cust_id.Text = "";
             textBox_cust_fname.Clear();
             textBox_cust_lname.Clear();
-            textBox_cust_phone_num.Clear();
-            textBox_cust_cc_num.Clear();
+            textBox_cust_phone.Clear();
+            textBox_cc_part1.Clear();
+            textBox_cc_part2.Clear();
+            textBox_cc_part3.Clear();
+            textBox_cc_part4.Clear();
             textBox_cust_balance.Clear();
+        }
+        private string getCustID()
+        {
+            // Authored By: Marcel Rico
+            string custID = "";
+            
+
+            try
+            {
+                sqlConnection1.Open();
+                SqlCommand cmd = sqlConnection1.CreateCommand();
+                cmd.CommandText = "SELECT MAX(cust_id) FROM CUSTOMER";
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+
+                    if (DBNull.Value.Equals(dr[0]))
+                    {
+                        custID = "1";
+                        Console.WriteLine(dr[0]);
+                    }
+                    else
+                    {
+                        custID = (int.Parse(dr[0].ToString()) + 1).ToString();
+                    }
+
+                }
+                sqlConnection1.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return custID;
         }
         private void changeButtonsEnabled(bool enable)
         {
@@ -59,32 +106,66 @@ namespace HotelApp_v1
         {
             this.Visible = false;
         }
-
         private void button_search_Click(object sender, EventArgs e)
         {
             changeButtonsEnabled(true); // delete this when above is filled out
         }
-
         private void button_create_Click(object sender, EventArgs e)
         {
-            emptyTextBoxes(); // clears texts from text boxes
-            changeCreateButtonsVisibility(false);
-            changeButtonsEnabled(false);
-            button_search.Enabled = false;
-            button_submit_create.Enabled = true;
-            changeCancelButton(false);
-        }
+            // Modified By: Marcel
+            changeTextBoxesReadOnlyStatus(false);
+            textBox_cust_fname.Focus();
 
+            textBox_cust_id.Text = getCustID();
+            textBox_cust_id.Visible = true;
+            button_search.Enabled = false;
+            
+            changeCreateButtonsVisibility(false);
+        }
         private void button_submit_create_Click(object sender, EventArgs e)
         {
-            emptyTextBoxes(); // clears texts from text boxes
-            changeCreateButtonsVisibility(true);
-            changeButtonsEnabled(true);
-            button_search.Enabled = true;
-            button_submit_create.Enabled = false;
-            changeCancelButton(false);
-        }
+            // Modified By: Marcel
 
+            string cust_first_name = textBox_cust_fname.Text;
+            string cust_last_name = textBox_cust_lname.Text;
+            string cust_phone_num = textBox_cust_phone.Text;
+            string cust_cc_num = textBox_cc_part1.Text+
+                                 textBox_cc_part2.Text+
+                                 textBox_cc_part3.Text+
+                                 textBox_cc_part3.Text;
+            string cust_balance = textBox_cust_balance.Text;
+
+            // Data Validation
+
+            sqlConnection1.Open();
+            SqlCommand cmd = sqlConnection1.CreateCommand();
+
+            // Create Credit Card Entry
+            cmd.CommandText = "INSERT INTO CREDIT_CARD (CREDIT_CARD_NO,CREDIT_CARD_FNAME,CREDIT_CARD_LNAME) VALUES ('"+cust_cc_num+"','"+cust_first_name+"','"+cust_last_name+"')";
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+
+            
+            // Create Customer Entry
+            cmd.CommandText = "INSERT INTO CUSTOMER (CUST_FNAME,CUST_LNAME,CUST_PHONE,CUST_BALANCE,CUST_CREDIT_CARD_NO) VALUES ('"+cust_first_name+"','"+cust_last_name+"','"+cust_phone_num+"',"+Decimal.Parse(cust_balance)+",'"+cust_cc_num+"')";
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            
+
+            sqlConnection1.Close();
+
+            MessageBox.Show("1 Credit Card and Customer Added");
+            changeTextBoxesReadOnlyStatus(true);
+
+
+            emptyTextBoxes();
+            changeCreateButtonsVisibility(true);
+
+            textBox_cust_id.Text = "";
+            textBox_cust_id.Visible = false;
+            comboBox_cust_id.Focus();
+
+        }
         private void button_edit_Click(object sender, EventArgs e)
         {
             changeEditButtonsVisibility(false);
@@ -94,7 +175,6 @@ namespace HotelApp_v1
             button_search.Enabled = false; // disable search button
             changeCancelButton(false);
         }
-
         private void button_submit_edit_Click(object sender, EventArgs e)
         {
             changeEditButtonsVisibility(true);
@@ -104,14 +184,12 @@ namespace HotelApp_v1
             button_search.Enabled = true; // enable search button
             changeCancelButton(false);
         }
-
         private void button_delete_Click(object sender, EventArgs e)
         {
             emptyTextBoxes(); // clears texts from text boxes
             changeButtonsEnabled(false); // hide edit and delete buttons
             changeTextBoxesReadOnlyStatus(true); // make text boxes read-only
         }
-
         private void button_cancel_Click(object sender, EventArgs e)
         {
             changeButtonsEnabled(false); // delete this when above is filled out
@@ -123,5 +201,35 @@ namespace HotelApp_v1
             button_submit_create.Enabled = false;
             button_search.Enabled = true; // enable search button
         }
+
+        private void textBox_cc_part1_TextChanged(object sender, EventArgs e)
+        {
+            if (this.textBox_cc_part1.Text.Length > 3)
+            {
+                GetNextControl(this.textBox_cc_part1, true).Focus();
+            }
+        }
+        private void textBox_cc_part2_TextChanged(object sender, EventArgs e)
+        {
+            if (this.textBox_cc_part2.Text.Length > 3)
+            {
+                GetNextControl(this.textBox_cc_part2, true).Focus();
+            }
+        }
+        private void textBox_cc_part3_TextChanged(object sender, EventArgs e)
+        {
+            if (this.textBox_cc_part3.Text.Length > 3)
+            {
+                GetNextControl(this.textBox_cc_part3, true).Focus();
+            }
+        }
+        private void textBox_cc_part4_TextChanged(object sender, EventArgs e)
+        {
+            if (this.textBox_cc_part4.Text.Length > 3)
+            {
+                GetNextControl(this.textBox_cc_part4, true).Focus();
+            }
+        }
+
     }
 }
